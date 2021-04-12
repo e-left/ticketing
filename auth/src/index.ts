957ef802f5184a9431808d90af7d3a78fa40b6ec
e-left <eleft@protonmail.com>
2021-04-12 12:22:08 +0300

    Update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,11 @@ import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+// yo
+
+app.get("/", (req, res) => {
+    res.send("this shit works");
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
