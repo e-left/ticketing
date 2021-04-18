3ffef7ca11c478751236cd06a997144650e09e7a
e-left <eleft@protonmail.com>
2021-04-19 02:31:53 +0300

    progress

---
+++ auth/src/models/user.ts
@@ -0,0 +1,48 @@
+import mongoose, { MongooseDocument } from 'mongoose';
+import { Password } from './../services/password';
+
+// interface that describes user model attributes
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// interface that describes user model properties
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// interface that describes user document properties
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save', async function(done)  {
+    if(this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+
+    done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
